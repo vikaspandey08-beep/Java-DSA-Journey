e20b75f7b5b721aa1ba86c418262ca30c8cc3846
vikaspandey08-beep <vippandey08@gmail.com>
2026-09-14 18:34:58 +0530

    Added Butterfly pattern code

---
+++ Pattern-Printings/Patterns.java
@@ -0,0 +1,38 @@
+// NESTED LOOPS FOR BUTTERFLY PATTERN 
+public class Patterns{
+       public static void Printpattern(int n){
+        int i;
+        int j;
+        for(i=1; i<=n; i++) {
+           for(j=1; j<=i; j++) {
+            System.out.print("*");
+        }
+         int spaces = 2 * (n-i);
+           for(j=1; j<=spaces; j++) {
+            System.out.print(" ");
+         }
+         for(j=1; j<=i; j++){
+            System.out.print("*");
+         }
+         System.out.println();
+        }
+
+        for(i=n; i>=1; i--) {
+           for(j=1; j<=i; j++) {
+            System.out.print("*");
+        }
+         int spaces = 2 * (n-i);
+         for(j=1; j<=spaces ;j++) {
+            System.out.print(" ");
+         }
+         for(j=1; j<=i ;j++) {
+            System.out.print("*");
+         }
+         System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int n = 5;
+        Printpattern(n);
+    }
+}
