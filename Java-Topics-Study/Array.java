c8911dd685e7696a988e4c575334082a80458fe1
Vikas Pandey <vippandey08@gmail.com>
2026-09-15 20:06:29 +0530

    Array Topics Basic study

---
+++ Java-Topics-Study/Array.java
@@ -0,0 +1,25 @@
+// ARRAYS:
+
+import java.util.Scanner;
+
+public class Array {
+   
+ public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+      int[] marks = new int[3];
+      marks[0] = 95; // physics
+      marks[1] = 97; // chemistry
+      marks[2] = 98; // english
+        
+System.out.println("YOUR PHYSICS MARKS ARE :");
+System.out.println(marks[0]);
+
+ System.out.println("YOUR CHEMISTRY MARKS ARE :");
+  System.out.println(marks[1]);
+
+  System.out.println("YOUR ENGLSIH MARKS ARE :");
+    System.out.println(marks[2]);
+
+        sc.close();
+    }
+ }
