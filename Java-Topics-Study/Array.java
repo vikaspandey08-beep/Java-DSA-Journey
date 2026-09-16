e1aeb8099a58c8476ecff5875dad9be1eb6ca522
Vikas Pandey <vippandey08@gmail.com>
2026-09-16 20:37:23 +0530

    Code Optimized

--- Java-Topics-Study/Array.java
+++ Java-Topics-Study/Array.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Array {
    
+  // Code optimized.
  public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
       int[] marks = new int[3];
