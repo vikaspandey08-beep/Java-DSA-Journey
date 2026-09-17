03419effe6a0b49fa432c08511b0428a5f0bc7d1
Vikas Pandey <vippandey08@gmail.com>
2026-09-17 19:03:31 +0530

    Sorting Topic

---
+++ Java-Topics-Study/Sorting.java
@@ -0,0 +1,29 @@
+// Sorting in java :
+// Bubble sort :
+
+import java.util.Scanner;
+
+public class Sorting{
+    public static void printArray(int arr[]){
+               for(int i=0;i<arr.length;i++){
+                        System.out.print(arr[i]+ " ");
+             }
+              System.out.println();
+      }
+      public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         int arr[] = {7,8,3,1,2};
+
+         for(int i=0;i<arr.length-1;i++){
+           for(int j =0;j<arr.length-i-1;j++){ //n = arr.length
+          if(arr[j]>arr[j+1]){
+               int temp = arr[j];
+              arr[j] = arr[j+1];
+              arr[j+1] = temp;
+          }
+          }
+        }
+        printArray(arr);
+ }
+}
+
--- Java-Topics-Study/Stringbuilder.java
+++ Java-Topics-Study/Stringbuilder.java
@@ -3,29 +3,29 @@
 import java.util.Scanner;
 
 public class Stringbuilder {
-   public static void main(String[] args){
+public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
          StringBuilder sb = new StringBuilder("Vikas");
          System.out.println(sb);
 
          System.out.println(sb.charAt(0));
 
-        // set char at 0:
+// set char at 0:
         sb.setCharAt(0,'B');
          System.out.println(sb);
 
          sb.setCharAt(5,'h');
          System.out.println(sb);
 
-        // insert element at index:
+// insert element at index:
          sb.insert(0,'S');
         System.out.println(sb);
 
-       //   delete the extra added 'S' :
+//   delete the extra added 'S' :
          sb.delete(0,1);
          System.out.println(sb);
 
-       //  append the new letters:
+//  append the new letters:
          sb.append(" ");
          sb.append("P");
          sb.append("a");
@@ -35,7 +35,7 @@ public class Stringbuilder {
          sb.append("y");
          System.out.println(sb);
 
-        // .length function for StringBuilder in java:
+// .length function for StringBuilder in java:
          sb.append(" ");
          sb.append("P");
          sb.append("a");
@@ -45,7 +45,7 @@ public class Stringbuilder {
          sb.append("y");
         System.out.println(sb.length());
 
-       //  Reverse a string in StringBuilder in java:
+ //  Reverse a string in StringBuilder in java:
          for(int i=0;i<sb.length()/2;i++){
              int front = i;
              int back = sb.length()-1-i;
@@ -56,5 +56,6 @@ public class Stringbuilder {
              sb.setCharAt(back, frontChar);
          }
          System.out.println(sb);
+         sc.close();
     }
 }
