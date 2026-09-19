bb3ea3f40036ae6ecdbd30e24e103a717dc23207
Vikas Pandey <vippandey08@gmail.com>
2026-09-19 13:49:08 +0530

    Recursion Example no 3

---
+++ Java-Topics-Study/Recursion3.java
@@ -0,0 +1,28 @@
+// Print x raise to the power n (Stack height = logn) :
+
+import java.util.Scanner;
+
+public class Recursion3{
+    public static int calcPower(int x,int n){
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+       if(n % 2 == 0){
+       return  calcPower(x,n/2) * calcPower(x,n/2);
+       }else {
+        return calcPower(x,n/2) * calcPower(x,n/2) * x;
+       }
+    }
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int x = 2;
+        int n = 5;
+        int ans =  calcPower(x,n);
+        System.out.println("Your Output is :");
+        System.out.println(ans);
+    sc.close();
+    }
+}
